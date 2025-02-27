781f142b28bdfd571eeed386de987b34abf7fbcd
Viktor Shevchuk <viktor.shevchuk.dev@gmail.com>
2025-02-27 15:39:36 +0100

    feat: integrate p-queue to queue reqs

--- server/src/controllers/candidates.ts
+++ server/src/controllers/candidates.ts
@@ -1,13 +1,21 @@
 import { RequestHandler } from "express";
+import PQueue from "p-queue";
 
 import { ctrlWrapper } from "../helpers";
 import { getCandidates } from "../services";
 
+const queue = new PQueue({ concurrency: 1 });
+
 const getAll: RequestHandler = async (req, res, next) => {
-  await getCandidates(res);
-  res.setHeader("Content-Type", "text/csv");
-  res.setHeader("Content-Disposition", 'attachment; filename="candidates.csv"');
-  res.setHeader("Content-Encoding", "br");
+  await queue.add(async () => {
+    await getCandidates(res);
+    res.setHeader("Content-Type", "text/csv");
+    res.setHeader(
+      "Content-Disposition",
+      'attachment; filename="candidates.csv"'
+    );
+    res.setHeader("Content-Encoding", "br");
+  });
 };
 
 export default {
